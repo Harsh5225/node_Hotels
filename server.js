7da904705975e08841033418be85d561f69dadb4
Harsh <harshsingh94070@gmail.com>
2024-07-04 18:22:47 +0530

    env file

--- server.js
+++ server.js
@@ -5,15 +5,21 @@ const personRoutes = require('./routes/personRoutes');
 const MenuItem=require('./routes/menuRoutes');
 const db=require('./db')
 app.use(bodyParser.json());
-
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
 app.get('/',(req,res)=>{
   res.send('Welcome to my hotel');
 })
-// Use the person routes
+// Use the person 
+
+
 app.use('/person', personRoutes);
 app.use('/menu',MenuItem);
+
 // Uncommented app.listen
-app.listen(3000, () => {
-  console.log('Listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
+
+
